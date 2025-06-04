213761acc0457cb28840db6c12138c8ab1a87f53
aidena18 <aiden.annis06@gmail.com>
2025-06-04 15:36:43 -0400

    Add files via upload

---
+++ Scripts/MemoryBubble.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;//newest
+using UnityEngine.Video;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class MemoryBubble : MonoBehaviour, IMixedRealityPointerHandler
+{
+    public string clipUrl;
+    public VideoScreenManager screenManager;
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        screenManager.PlayClip(clipUrl);  // This should open the fullscreen panel
+        Debug.Log("🟣 Bubble clicked: opening fullscreen video.");
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData) { }
+    public void OnPointerUp(MixedRealityPointerEventData eventData) { }
+    public void OnPointerDragged(MixedRealityPointerEventData eventData) { }
+}
